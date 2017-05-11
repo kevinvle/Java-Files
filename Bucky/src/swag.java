95a47b2a773e21ac29edf1b2b6926bcf63df93aa
Kevin Viet Le <kevin1le@hotmail.com>
2017-05-11 01:19:14 -0700

    nothing

--- Bucky/src/swag.java
+++ Bucky/src/swag.java
@@ -1,4 +1,14 @@
 
-public class swag {
-
+public class swag {	
+	public static void main(String[] args) {
+		int bucky[] = {21,16,86,21,3};
+		int sum=0;
+		
+		for ( int counter =0; counter < bucky.length; counter++) {
+			sum += bucky[counter]; 
+			
+		}
+	}
+	
+	
 }
